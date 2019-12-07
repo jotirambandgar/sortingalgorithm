4304c63d07d29b8815bdcfaaf8fbe11728566cf4
jotirambandgar <bandgar09@gmail.com>
2019-12-07 17:46:05 +0530

    stored : dictonary values into array

--- arithematicOperation.sh
+++ arithematicOperation.sh
@@ -3,6 +3,7 @@ read -p "Enter value of a" a
 read -p "Enter value of b" b
 read -p "Enter value of c" c
 declare -A result;
+count=0
 operation1=$(( $a + $b * $c ))
 echo "a+b*c=$operation1"
 operation2=$(( $a * $b + $c ))
@@ -15,5 +16,11 @@ result["result1"]=$operation1
 result["result2"]=$operation2
 result["result3"]=$operation3
 result["result4"]=$operation4
-echo "dictionar----------> ${result[@]}"
+resultArr[(count++)]=${result["result1"]}
+resultArr[(count++)]=${result["result2"]}
+resultArr[(count++)]=${result["result3"]}
+resultArr[(count++)]=${result["result4"]}
+
+
+echo "dictionar----------> ${resultArr[@]}"
 
