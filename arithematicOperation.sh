3a012f03088474fd41f901a19c666564b3969df5
jotirambandgar <bandgar09@gmail.com>
2019-12-07 17:37:23 +0530

    added:dictonary for storing results

--- arithematicOperation.sh
+++ arithematicOperation.sh
@@ -2,6 +2,7 @@
 read -p "Enter value of a" a
 read -p "Enter value of b" b
 read -p "Enter value of c" c
+declare -A result;
 operation1=$(( $a + $b * $c ))
 echo "a+b*c=$operation1"
 operation2=$(( $a * $b + $c ))
@@ -10,3 +11,9 @@ operation3=$(( $c + $a / $b ))
 echo "c+a/b=$operation3"
 operation4=$(( $a % $b + $c ))
 echo "a%b+c=$operation4"
+result["result1"]=$operation1
+result["result2"]=$operation2
+result["result3"]=$operation3
+result["result4"]=$operation4
+echo "dictionar----------> ${result[@]}"
+
